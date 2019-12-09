a1001ed0e6e5c66f9439276105973d3034d0a41c
tfranks247 <tfranks247@gmail.com>
2019-12-09 16:48:11 -0500

    final push

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,22 +1,30 @@
 package com.example.demo;
 
+import com.cloudinary.utils.ObjectUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
+import java.io.IOException;
 import java.security.Principal;
+import java.util.Map;
 
 @Controller
 public class HomeController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    MessageRepository messageRepository;
+
+    @Autowired
+    CloudinaryConfig cloudc;
+
 
     @GetMapping("/register")
     public String showRegistrationPage(Model model) {
@@ -56,5 +64,65 @@ public class HomeController {
     String username = principal.getName();
     model.addAttribute("user", userRepository.findByUsername(username));
         return "secure";
+    }
+
+    /////////////////// Copied in all of other HC
+
+    @RequestMapping("/list")
+    public String listMessages(Model model){
+        model.addAttribute("messages", messageRepository.findAll());
+        return "list";
+    }
+
+    @GetMapping("/add")
+    public String messageForm(Model model){
+        model.addAttribute("message", new Message());
+        return "messageform";
+    }
+
+    @PostMapping("/process")
+    public String processForm( @ModelAttribute Message message1, @Valid  Message message, BindingResult result,
+                               @RequestParam("file") MultipartFile file
+    ) {
+        if (result.hasErrors()) {
+            return "messageform";
+        }
+        if (file.isEmpty() && message1.getImage() == null) {
+            return "redirect:/add";
+        }
+        if (!file.isEmpty()) {
+            try {
+                Map uploadResult = cloudc.upload(file.getBytes(),
+                        ObjectUtils.asMap("resourcetype", "auto"));
+                message.setImage(uploadResult.get("url").toString());
+                messageRepository.save(message);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "redirect:/add";
+            }
+        }
+        else
+            messageRepository.save(message1);
+        return "redirect:/list";
+    }
+
+    @RequestMapping("/list/{id}")
+    public String showMessage(@PathVariable("id") long id, Model model)
+    {
+        model.addAttribute("message", messageRepository.findById(id).get());
+        return "show";
+    }
+
+    @RequestMapping("/update/{id}")
+    public String updateMessage(@PathVariable("id") long id, Model model) {
+        model.addAttribute("message", messageRepository.findById(id).get());
+        return "messageform";
+    }
+
+    @RequestMapping("/delete/{id}")
+    public String delmessage(@PathVariable("id") long id) {
+        messageRepository.deleteById(id);
+        return "/list";                             /////////////// changed from redirect:/
     }
 }
---
+++ src/main/java/com/example/demo/Message.java
@@ -0,0 +1,71 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String image;
+
+    @NotNull
+    @Size(min=1)
+    private String content;
+
+    @NotNull
+    @Size(min=5)
+    private String date;
+
+    @NotNull
+    @Size(min=1)
+    private String sentBy;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getSentBy() {
+        return sentBy;
+    }
+
+    public void setSentBy(String sentBy) {
+        this.sentBy = sentBy;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+}
